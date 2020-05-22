b067677a28fb39d301fb33eb5cd927898953f4c1
Ibrahimhussien227 <65757724+Ibrahimhussien227@users.noreply.github.com>
2020-05-22 12:49:39 +0300

    Update ProfileActivity.kt

--- app/src/main/java/com/example/myapplication/ProfileActivity.kt
+++ app/src/main/java/com/example/myapplication/ProfileActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_profile.*
 import kotlinx.android.synthetic.main.activity_registration.*
 
